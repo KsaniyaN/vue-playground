0caf14bc8b8f414485a23f3275582ad249edd2f2
XeniaN <xenia.novosilska@mirus.group>
2021-10-15 06:30:21 +1100

    tests - check if element doesn't exist - find

--- src/components/tests/unit/Nav.spec.js
+++ src/components/tests/unit/Nav.spec.js
@@ -3,9 +3,10 @@ import Nav from "../../Nav.vue";
 
 describe("Nav.vue", () => {
 
+    // check if element exists - get
     it("renders a profile link", () => {
         const wrapper = mount(Nav, {
-            data(){
+            data() {
                 return {
                     isLoggedIn: true,
                 };
@@ -14,4 +15,18 @@ describe("Nav.vue", () => {
         const profileLink = wrapper.get("#profile");
         expect(profileLink.text()).toEqual("My Profile");
     });
+
+    // check if element doesn't exist - find
+    it("should not render a profile link", () => {
+        const wrapper = mount(Nav, {
+            data() {
+                return {
+                    isLoggedIn: false,
+                };
+            },
+        });
+        const profileLink = wrapper.find("#profile");
+        expect(profileLink.exists()).toBe(false);
+    });
+
 });

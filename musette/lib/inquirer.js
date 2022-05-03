2c1744468e947018ba5bfe35123d7816917b746f
XeniaN <!Tratata1>
2022-05-03 15:49:49 +1000

    prompt the user for repo details; create a repo - get Url

--- musette/lib/inquirer.js
+++ musette/lib/inquirer.js
@@ -1,5 +1,5 @@
 const inquirer = require('inquirer');
-const minimist = require('minimist');
+const minimist = require('minimist');   // setting defaults for the prompts
 const files = require('./files.js');
 
 module.exports = {
@@ -30,7 +30,44 @@ module.exports = {
                 }
             }
         ];
-
         return inquirer.prompt(questions);
-    }
+    },
+
+    askRepositoryDetails: () => {
+        // 0 - path to the node itself
+        // 1 - path to script
+        const argv = require('minimist')(process.argv.slice(2));
+        const questions = [
+            {
+                type: 'input',
+                name: 'name',
+                message: 'Please enter a name for your repository',
+                default: argv._[0] || files.getCurrentDirectoryBase(),
+                validate: function (value) {
+                    if (value.length) {
+                        return true;
+                    } else {
+                        return 'Please enter a unique name for the repository.'
+                    }
+                }
+            },
+            {
+                type: 'input',
+                name: 'description',
+                default: argv._[1] || null,
+                message: 'Set description of the repo (optional)'
+            },
+            {
+                type: 'input',
+                name: 'visibility',
+                message: 'Would you like this repo to be public or private?',
+                choices: [
+                    'public',
+                    'private'
+                ],
+                default: 'public'
+            }
+        ];
+        return inquirer.prompt(questions);
+    },
 }
